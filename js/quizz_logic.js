1204a6ed499717c128148e16cc2c22a72b48a620
Eugen Jercan <jercan.ionut@gmail.com>
2016-10-17 08:06:27 +0300

    Add answer validation
    Prevent the user to go to the next question if no answer was selected
    for current question.

--- js/quizz_logic.js
+++ js/quizz_logic.js
@@ -8,32 +8,25 @@ var quizzQuestions = [{img: "img/geti.jpg", question: "Cum îi numeau grecii pe 
 					  {img: "img/neagoe-basarab.jpg", question: "Învățăturile lui Neagoe Basarab îi erau adresate lui:", choices: ["Sava", "Teodosie", "Teofil"], correctAnswer: 1},
 					  {img: "img/vlad_tepes.jpg", question: "În noaptea zilei de 17 iunie 1462, Vlad Țepeș s-a luptat cu:", choices: ["Baiazid", "Soliman Pașa", "Suleiman Magnificul", "Mahomed al II-lea"], correctAnswer: 3},
 					  {img: "img/mihai_viteazu.jpg", question: "În ce an a avut loc unirea înfăptuită de Mihai Viteazul?", choices: ["1918", "1456", "1688", "1600", "1859"], correctAnswer: 3},
-					  {img: "img/constantin-brancoveanu.jpg", question: "Constantin Brâncoveanu a construit mănăstirea:", choices: ["Putna", "Hurezi", "Cozia", "Antim", "Curtea de Argeș"], correctAnswer: 1}];
-// Store location for feat-photo element
-var featPhoto = document.getElementById("feat-photo");
-// Store location for current question indicator element 
-var currentQuestion = document.getElementById("cQ");
-// Store location for questions number indicator element
-var questionsNumber = document.getElementById("qN");
-// Store location for question container element
-var questionElement = document.getElementById("question");
-// Store location for answers container
-var answers = document.getElementById("answers");
-// Store location for submit button
-var submitButton = document.getElementById("send");
-// Set initial question to 0;
-var questionNo = 0;
-// Set correct answers to 0
-var correctAnswers = 0;
+					  {img: "img/constantin-brancoveanu.jpg", question: "Constantin Brâncoveanu a construit mănăstirea:", choices: ["Putna", "Hurezi", "Cozia", "Antim", "Curtea de Argeș"], correctAnswer: 1}],
+	featPhoto = document.getElementById("feat-photo"), // Store location for feat-photo element
+	currentQuestion = document.getElementById("cQ"), // Store location for current question indicator element 
+	questionsNumber = document.getElementById("qN"), // Store location for questions number indicator element
+	questionElement = document.getElementById("question"), // Store location for question container element
+	answers = document.getElementById("answers"), // Store location for answers container
+	errorContainer = document.getElementById("errorContainer"), // Store location of error container 
+	submitButton = document.getElementById("send"), // Store location for submit button
+	questionNo = 0, // Set initial question to 0;
+	correctAnswers = 0; // Set correct answers to 0
 
 // write questions number
 questionsNumber.textContent = quizzQuestions.length;
 
 // add answers
-function addAnswers(qNo) {
+function insertAnswers(qNo) {
 	var html = "";
 
-	for (var i = 0; i < quizzQuestions[qNo].choices.length; i++) {
+	for (var i = 0; i < quizzQuestions[qNo].choices.length; i++) { // iterate throw answers array and build radio inputs
 		html += "<div class='answer-container'><label for='answer" + i + "'><input type='radio' id='answer" + i + "' name='answer' value='" + quizzQuestions[qNo].choices[i] + "'>" + quizzQuestions[qNo].choices[i] + "</label></div>";
 	}
 	return html;
@@ -44,45 +37,42 @@ function populateQuizz() {
 	console.log(questionNo);
 	
 	var url = "url(" + quizzQuestions[questionNo].img + ")"; // store the url for feat-photo
-	// set the url for feat-photo
-	featPhoto.style.backgroundImage = url;
+	featPhoto.style.backgroundImage = url; // set the url for feat-photo
 
-	// set current question number
-	currentQuestion.textContent = questionNo + 1;
+	currentQuestion.textContent = questionNo + 1; // upgrade question number indicator
 
-	// set the question
-	questionElement.textContent = quizzQuestions[questionNo].question;
+	questionElement.textContent = quizzQuestions[questionNo].question; // insert current question
 
-	// set the answers
-	answers.innerHTML = addAnswers(questionNo);
+	answers.innerHTML = insertAnswers(questionNo); // insert answers
 
-	// increment question number
-	questionNo += 1;
+	questionNo += 1; // upgrade question number
 }
 
 // get user answers
 function getUserAnswer() {
 	console.log("user answer");
+
 	// answer inputs
-	var answers = quizz.answer;
+	var answers = quizz.answer; // store the answers array
 	console.log(answers);
-	var userAnswer = quizz.answer.value;
+	var userAnswer = quizz.answer.value; // store the value of the selected answer
 	var userAnswerId = "";
 	console.log(userAnswer);
 
-	for (var i = 0; i < answers.length; i ++) {
+	for (var i = 0; i < answers.length; i ++) { // identify the id of the selected answer
 		if (userAnswer == answers[i].value) {
-			userAnswerId = answers[i].id.slice(-1);
+			userAnswerId = answers[i].id.slice(-1); // store only the number from the id
 			console.log(userAnswerId + " answer id");
 		}
 	}
 
-	if (userAnswerId == quizzQuestions[questionNo-1].correctAnswer) {
+	if (userAnswerId == quizzQuestions[questionNo-1].correctAnswer) { // check if the selected answer was the correct one
 		correctAnswers++;
 		console.log(correctAnswers + " raspuns corect");
 	}
 }
 
+// generate success or failure messages
 function generateMessage(cq) {
 	if (cq < quizzQuestions.length / 2) {
 		if (cq == 0) {
@@ -101,17 +91,26 @@ function generateMessage(cq) {
 	}
 }
 
+// Populate quizz when the player enters the page
 populateQuizz();
 
+// Add an event listener on the button
 submitButton.addEventListener('click', function(e) {
 	e.preventDefault();
-		
-	if (questionNo == quizzQuestions.length) {
-		getUserAnswer();
-		document.getElementById("quizzContent").innerHTML = '<div class="message-container">' + generateMessage(correctAnswers) + '</div>';
+
+	errorContainer.textContent = ""; // delete error message
+	console.log (quizz.answer.value);
+	
+	if (quizz.answer.value == "") { // check if the user submited an answer before clicking the button
+		errorContainer.textContent = "Selectează un răspuns!";
 	} else {
-		getUserAnswer();
-		populateQuizz();
+		if (questionNo == quizzQuestions.length) { // check if last question
+			getUserAnswer();
+			document.getElementById("quizzContent").innerHTML = '<div class="message-container">' + generateMessage(correctAnswers) + '</div>';
+		} else {
+			getUserAnswer();
+			populateQuizz();
+		}
 	}
 });
 
